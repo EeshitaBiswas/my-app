ba3ea8063566c2216d115b405284eafffda9825a
Eeshita <biswas@udel.edu>
2021-09-29 14:04:32 -0400

    same change but npm deployed first

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           React Tutorials
+           React Tutorials through VS Code
         </a>
       </header>
     </div>
