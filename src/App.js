b0af6bc538f63e8ec29095d0ccce94347c27c210
Eeshita <biswas@udel.edu>
2021-09-29 14:28:26 -0400

    button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { JSXElementConstructor } from 'react';
 
 function App() {
   return (
@@ -9,14 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           React Tutorials through VS Code but not through command line
-        </a>
+        <button>Click Me</button>
+        
       </header>
     </div>
   );
